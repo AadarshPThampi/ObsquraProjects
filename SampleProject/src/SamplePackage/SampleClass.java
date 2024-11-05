29b45a08c97fb7617c1ffcba90ea2ef0b6315921
AADARSH NANDAN <AADARSH NANDAN@LAPTOP-T9VL9J16>
2024-11-05 14:55:03 +0530

    1st commit

---
+++ SampleProject/src/SamplePackage/SampleClass.java
@@ -0,0 +1,50 @@
+package SamplePackage;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String S="Hello";
+		System.out.println(S);
+		
+		String M= new String("Hai");
+		System.out.println(M);
+
+		char ar[]= {'a','b','c','d','e','f','g'};
+		String P = new String(ar);
+		System.out.println(P);
+		
+		
+		int X = S.length();
+		System.out.println("Length of String is "+X);
+		
+		char C=S.charAt(4);
+		System.out.println("Char at 4th index is "+C);
+		
+		int a =10;
+		String New=String.valueOf(a); // used to convert any datatype to string
+		System.out.println(New);
+		
+		String ab="Kishore";			//Declared using String literals
+		String cd="Kishore";
+		System.out.println(ab==cd);
+		
+		String ef= new String("Kishore");	//Declared using new keyword
+		System.out.println(ef==ab);
+		
+		String ij="Hai";
+		String kl="hello";
+		String mn="hai";
+		String op="hai";
+		System.out.println("Is ij equal to mn: "+ij.equals(mn));
+		System.out.println("Is op equal to mn: "+op.equals(mn));
+		
+		System.out.println(ij.equalsIgnoreCase(mn));
+		
+		String ety="";
+		System.out.println(ety.isEmpty());
+		
+		
+	}
+
+}
---
+++ SampleProject/src/SamplePackage/StringBufferAndBuilder.java
@@ -0,0 +1,53 @@
+package SamplePackage;
+
+public class StringBufferAndBuilder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+StringBuffer a= new StringBuffer("Name");
+System.out.println(a);
+
+StringBuilder b = new StringBuilder("Place");
+System.out.println(b);
+
+//Append
+a.append(" is");
+System.out.println(a);
+
+b.append(" to go");
+System.out.println(b);
+
+//Insert
+a.insert(1, "Sample");
+System.out.println(a);
+
+b.insert(5, "need");
+System.out.println(b);
+
+//Replace
+a.replace(1, 2, "Hari");
+System.out.println(a);
+
+b.replace(5, 9, "we");
+System.out.println(b);
+
+//Delete
+a.delete(1, 10);
+System.out.println(a);
+
+b.delete(5, 7);
+System.out.println(b);
+
+//Reverse
+a.reverse();
+System.out.println(a);
+
+b.reverse();
+System.out.println(b);
+
+//concat, contains, to upper, to lower		
+	}
+
+}
